06cedb7d8c9122c023e074a5ea1ea443de99271e
yangzp <yangzp@shenghesun.com>
2018-08-13 11:24:02 +0800

    update

---
+++ mp-choir-common/src/main/java/com/shenghesun/entity/BaseEntity.java
@@ -0,0 +1,73 @@
+package com.shenghesun.entity;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Version;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.shenghesun.ai.entity.listener.BaseEntityListener;
+
+@MappedSuperclass
+@EntityListeners(value = {BaseEntityListener.class})
+public class BaseEntity implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2166750384646114468L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	protected Long id;
+	
+	@Version
+	protected Long version = 0l;
+	
+	@JsonFormat(timezone="GMT+8")
+	@Column(updatable=false, nullable=false)
+	protected Timestamp creation = new Timestamp(System.currentTimeMillis());
+	
+//	@Index(name="lastModified")
+	@JsonFormat(timezone="GMT+8")
+	@Column(nullable=false)
+	protected Timestamp lastModified = new Timestamp(System.currentTimeMillis());
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getVersion() {
+		return version;
+	}
+
+	public void setVersion(Long version) {
+		this.version = version;
+	}
+
+	public Timestamp getCreation() {
+		return creation;
+	}
+
+	public void setCreation(Timestamp creation) {
+		this.creation = creation;
+	}
+
+	public Timestamp getLastModified() {
+		return lastModified;
+	}
+
+	public void setLastModified(Timestamp lastModified) {
+		this.lastModified = lastModified;
+	}
+}
